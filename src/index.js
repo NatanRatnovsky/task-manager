9ae2acd6f8f8a560184f4f60e2a85eecc2923dbc
NatanRatnovsky <45706031+NatanRatnovsky@users.noreply.github.com>
2021-10-08 02:09:37 +0300

    Created User and Task backend

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-const express = require("express");
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(express.json);
-
-app.get('/users', (req, res) => {
-    res.send('get test!');
-})
-
-app.post('/users', (req, res) => {
-    console.log(req.body);
-    res.send('testing!');
-})
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
+
+
 
